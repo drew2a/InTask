867263b232e7355187ad7b7af218c22a92014f25
drew2a <for.drew@gmail.com>
2017-10-19 10:03:15 +0300

    add example

---
+++ snail.py
@@ -0,0 +1,36 @@
+import itertools
+
+
+def print_matrix(matrix):
+    for row in range(len(matrix)):
+        print(" ".join('{:^5}'.format(i) for i in matrix[row]))
+
+
+def create_snail_matrix(size):
+    matrix = [[0] * size for i in range(size)]
+    ix, iy = size - 1, 0
+    n = size * size
+    directions = itertools.cycle([
+        (0, 1),  # down
+        (-1, 0),  # left
+        (0, -1),  # up
+        (1, 0)])  # rigth
+
+    direction_change_count = 2
+    for i in range(size):
+        matrix[0][i] = n - i
+
+    n -= ix
+    for step in range(size - 1, 0, -1):
+        for direction_change in range(direction_change_count):
+            direction = next(directions)
+            for index in range(step):
+                ix, iy = ix + direction[0], iy + direction[1]
+                n -= 1
+                matrix[iy][ix] = n
+
+    return matrix
+
+
+_matrix = create_snail_matrix(10)
+print_matrix(_matrix)
