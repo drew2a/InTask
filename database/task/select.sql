755890a838293eaca9df10c4b395a563c0042871
drew2a <for.drew@gmail.com>
2017-10-25 16:43:20 +0300

    add sql task

--- database/task/select.sql
+++ database/task/select.sql
@@ -1,1 +1,9 @@
-select * from COMPUTERS
+-- select all departments with conditions:
+-- 1. all computers have ram>=8
+-- 2. department have only  MAC's
+select DEPARTMENT
+FROM COMPUTERS
+GROUP by DEPARTMENT
+HAVING min(RAM)>=8 AND
+count(*)=count(case when TYPE = 'MAC' then 1 else null end)
+
