1a63e6f073cd61ee6907aa5af0b9decd990fb5ee
drew2a <for.drew@gmail.com>
2017-10-26 10:26:13 +0300

    add bars google problem

--- task/array_tasks.py
+++ task/array_tasks.py
@@ -98,5 +98,69 @@ def max_sum_sub_array(given_array):
             yield item
 
 
-def merge(array1, array2, condition):
-    return list(filter(condition, array1)) + list(filter(condition, array2))
+def print_bars(bars, water):
+    max_bar = max(bars)
+    for h in range(max_bar, 0, -1):
+        line = ''
+        for index in range(len(bars)):
+            if bars[index] < h and water[index] < h:
+                line += '  '
+            elif water[index] >= h > bars[index]:
+                line += '~~'
+            else:
+                line += 'oo'
+            line += '|'
+
+        print(line)
+    line = ''
+    for i in range(len(bars)):
+        line += '{:^3}'.format(i)
+    print(line)
+
+
+def find_water_count(array):
+    if len(array) <= 2:
+        return 0
+    print('Given bars:')
+    print_bars(array, array)
+    water_array = list(array)
+    total_water_count = 0
+    extremes = find_extremes(array)
+
+    print('\nExtremes: {}'.format(extremes))
+
+    for extrem_index in range(len(extremes) - 1):
+        first_extrem = extremes[extrem_index]
+        second_extrem = extremes[extrem_index + 1]
+        min_extrem_value = min(first_extrem[1], second_extrem[1])
+        increment = 1 if first_extrem[0] < second_extrem[0] else -1
+        print('\nMove: {}->{}'.format(first_extrem[0], second_extrem[0]))
+        for array_index in range(first_extrem[0] + increment, second_extrem[0], increment):
+            if water_array[array_index] < min_extrem_value:
+                total_water_count += min_extrem_value - water_array[array_index]
+                water_array[array_index] = min_extrem_value
+
+        print_bars(array, water_array)
+
+    return total_water_count
+
+
+def find_extremes(array):
+    size = len(array)
+    if size <= 2:
+        return []
+
+    extremes = []
+    previous_value = 0
+    for i in range(size - 1):
+        current_value = array[i]
+        next_value = array[i + 1]
+        if (current_value > previous_value and next_value <= current_value) or \
+                (current_value == previous_value and next_value < current_value):
+            extremes.append((i, current_value))
+        previous_value = current_value
+
+    if array[size - 1] > array[size - 2]:
+        extremes.append((size - 1, array[size - 1]))
+
+    return sorted(extremes, key=lambda t: t[1], reverse=True)
