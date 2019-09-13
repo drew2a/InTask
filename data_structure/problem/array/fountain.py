4e3d00c04418a470b8f9dcaa66a4c0bfbc7a81b1
drew2a <for.drew@gmail.com>
2019-09-14 00:15:45 +0300

    add visualisation

--- data_structure/problem/array/fountain.py
+++ data_structure/problem/array/fountain.py
@@ -1,5 +1,22 @@
+def visualize_fountain(a, points):
+    fountains1 = ["---"] * len(a)
+    fountains2 = ["   "] * len(a)
+    for point in points:
+        fountains1[max(0, point - a[point])] = " .."
+        fountains2[max(0, point - a[point])] = "/  "
+        fountains1[min(point + a[point], len(a) - 1)] = ".. "
+        fountains2[min(point + a[point], len(a) - 1)] = "  \\"
+
+    for point in points:
+        fountains1[point] = "   "
+        fountains2[point] = "\|/"
+
+    print(" " + "".join(fountains1)[1:-1:])
+    print("".join(fountains2))
+    print(a)
+
+
 def found_min_required_fountains(a):
-    print("Array: {}".format(a))
     array_length = len(a)
     scores = []
     position_availability = [set() for _ in range(array_length)]
@@ -19,11 +36,11 @@ def found_min_required_fountains(a):
     # print("Max score: {}".format(max(scores, key=lambda element: element["score"])))
     # print("Sum: {}".format(sum(map(lambda element: element["score"], scores))))
     # print("Availability: {}".format(position_availability))
-    iterations = 0
+    points = []
     total_score = sum(map(lambda element: element["score"], scores))
     while True:
-        iterations += 1
         max_scored_item = max(scores, key=lambda element: element["score"])
+        points.append(max_scored_item["index"])
         for i in range(max_scored_item["left"], max_scored_item["right"]):
             for positions in position_availability[i]:
                 scores[positions]["score"] -= 1
@@ -32,11 +49,14 @@ def found_min_required_fountains(a):
         if total_score == new_total_score or new_total_score <= 0:
             break
         total_score = new_total_score
-
-    return iterations
+    print("\n")
+    visualize_fountain(a, points)
+    return len(points)
 
 
 print("Result " + str(found_min_required_fountains([1, 1, 1])))
 print("Result " + str(found_min_required_fountains([1, 1, 1, 1, 1])))
 print("Result " + str(found_min_required_fountains([1, 1, 2, 1, 1])))
+print("Result " + str(found_min_required_fountains([3, 1, 1, 2, 1, 0, 4, 0, 0, 3])))
+print("Result " + str(found_min_required_fountains([3, 1, 1, 2, 1, 0, 2, 0, 0, 3])))
 print("Result " + str(found_min_required_fountains([1, 1, 2, 1, 3, 1, 1, 1, 1, 4, 1, 1, 1, 1, 3, 1, 2, 1, 1])))
